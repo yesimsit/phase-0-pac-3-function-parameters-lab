b94b56cee3cef12bd99aefbedfa48f48f78ed1e8
yesim <sityesim@gmail.com>
2022-04-20 02:07:21 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction(name){
+    
+    return `Hi, my name is ${name}.`
+}
+introduction("Aki");
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguage("Aki","Ember.js");
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+  return  `Hi, my name is ${name} and I am learning to program in ${language}.`
+
+}
+introductionWithLanguageOptional("Gracia");
